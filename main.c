a791eebbe82dbfd71f17294fb287217490d4018c
Marc Te <marcte@vps-8762281a.vps.ovh.net>
2021-04-20 09:31:54 +0000

    final commit

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
-#include "inc/libasm.h"
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+typedef struct	s_list
+{
+	void		*content;
+	struct s_list	*next;
+}		t_list;
+
+
+size_t	ft_strlen(char *s);
+char	*ft_strcpy(char *d, char *s);
+int	ft_strcmp(char *s1, char *s2);
+int	ft_write(int fd, char *s, int len);
+int	ft_read(int fd, char *s, int len);
+char	*ft_strdup(char *s);
+int	ft_list_size(t_list *lst);
+t_list	*ft_list_push_front(t_list **alst, t_list *new);
 
 
 t_list	*ft_lstnew(void *content)
